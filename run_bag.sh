de78ec88144118b9ef98ada5bf8871e26b3edbca
MonsieurTusk <83382059+MonsieurTusk@users.noreply.github.com>
2023-07-28 12:35:29 -0400

    Add files via upload

---
+++ run_bag.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+xterm -e "source /opt/ros/humble/setup.bash; ros2 bag play bag_files/bag_lidar/ --loop" &
+sleep 1
+xterm -e "source /opt/ros/humble/setup.bash; colcon build; source install/setup.bash; ros2 run talker_listener talkerNode_m1m2" &
+sleep 15
+xterm -e "source /opt/ros/humble/setup.bash; source install/setup.bash; ros2 run talker_listener listenerNode" & 
+sleep 1 
+xterm -e "rviz2 -d talker_listener/PFE_PointCloud2.rviz"
+sleep 1
