ce5ca74976680b6473f032ee930b4ad7c6bc243c
MonsieurTusk <83382059+MonsieurTusk@users.noreply.github.com>
2023-07-29 22:11:14 -0400

    Creation des points avec rviz

--- talker_listener/talker_listener/listener_node.py
+++ talker_listener/talker_listener/listener_node.py
@@ -23,42 +23,55 @@ class ListenerNode(Node):
     def __init__(self):
         super().__init__("listener_node")
         self.msg_lidar = None  # Initialiser msg_lidar à None
+        self.msg_moteur1 = None
+        self.msg_moteur2 = None
 
-        self.subscription = self.create_subscription(
-            Float32,
-            '/topic_lidar',  # Attribuer le bon topic à celui voulu
-            self.listener_callback,
-            10
-        )
+        #Ecoute du topic /topic_lidar pour obtenir les donnees du lidar
+        self.subscription_lidar = self.create_subscription(Float32, '/topic_lidar', self.listener_callback_lidar, 10)
+
+        #Ecoute du topic /topic_moteur1 pour obtenir les donnees du moteur1
+        self.subscription_m1 = self.create_subscription(Float32, '/topic_moteur1', self.listener_callback_m1, 10)
+
+        #Ecoute du topic /topic_moteur2 pour obtenir les donnees du moteur2
+        self.subscription_m2 = self.create_subscription(Float32, '/topic_moteur2', self.listener_callback_m2, 10)
         
-        self.publisher_ = self.create_publisher(PointCloud2, '/points2', 10)  # Attribuer le bon topic à celui voulu
+        self.publisher_ = self.create_publisher(PointCloud2, '/points2', 12)  # Attribuer le bon topic à celui voulu
         timer_period = 0.01  # le mettre à une seconde (avant : 0.01s)
         self.timer = self.create_timer(timer_period, self.timer_callback)
     
-    def listener_callback(self, msg):
-        self.get_logger().info(f"Received {msg.data}")
+    def listener_callback_lidar(self, msg):
+        self.get_logger().info(f"Lidar received {msg.data}")
         self.msg_lidar = msg.data  # Stocker la valeur msg.data dans msg_lidar
 
+    def listener_callback_m1(self, msg):
+        self.get_logger().info(f"M1 received {msg.data}")
+        self.msg_moteur1 = msg.data  # Stocker la valeur msg.data dans msg_lidar
+
+    def listener_callback_m2(self, msg):
+        self.get_logger().info(f"M2 received {msg.data}")
+        self.msg_moteur2 = msg.data  # Stocker la valeur msg.data dans msg_lidar
+
     def timer_callback(self):
-        if self.msg_lidar is not None:
+        if self.msg_lidar is not None and self.msg_moteur1 is not None and self.msg_moteur2 is not None:
             msg_pc2 = PointCloud2()
-            msg_pc2 = self.publishPC2(self.msg_lidar)
+
+            #Envoie des valeurs du 
+            msg_pc2 = self.publishPC2(self.msg_lidar, self.msg_moteur1, self.msg_moteur2)
             self.publisher_.publish(msg_pc2)
             self.get_logger().info('Valeur POINTCLOUD envoyee')
 
 
-    def publishPC2(self, count):
-        #x, y = np.meshgrid(np.linspace(-2, 2, width), np.linspace(-2, 2, height))
-        x = 10.0
-        y = 20.0
-        z = 30.0  # 0.5 * np.sin(2 * x - count / 10.0) * np.sin(2 * y)
+    def publishPC2(self, lidar_data, moteur1_data, moteur2_data):
+        #Initialisation des donnees
+        angle_origine_m1 = 90.0
+        angle_origine_m2 = 90.0
 
-        # Convertir x, y et z en tableaux NumPy avant d'appeler flatten()
-        #x = np.array(x)
-        #y = np.array(y)
-        #z = np.array(z)
         
-        #points = np.array([x.flatten(), y.flatten(), z.flatten()]).T
+        #Calcul des angles pour le PointCloud
+        x = lidar_data * np.sin(moteur2_data - angle_origine_m2)
+        y = lidar_data * np.sin(moteur1_data - angle_origine_m1)
+        z = lidar_data * np.abs(np.cos(moteur1_data - angle_origine_m1) * np.cos(moteur2_data - angle_origine_m2))
+        
         points = np.array([(x,y,z)], dtype=[('x', np.float32), ('y', np.float32), ('z', np.float32)])  # Point 3 avec coordonnées XYZ et couleur RGB
         
         # Créer le message PointCloud2
@@ -68,9 +81,11 @@ class ListenerNode(Node):
         pcl_msg = PointCloud2()
         pcl_msg.header = header
         pcl_msg.height = 1
-        pcl_msg.width = len(points)
+        pcl_msg.width = 1
         pcl_msg.is_dense = False
         pcl_msg.is_bigendian = False
+        pcl_msg.point_step = 12 #3 * 4 bytes
+        pcl_msg.row_step = 12
         
         fields = [
                PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
@@ -78,28 +93,8 @@ class ListenerNode(Node):
                PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1)
            ]
 
-        #header = Header()
-        #header.frame_id = "map"
-
-
-
         # Créer un nuage de points avec les coordonnées x, y, z
         #points = [(float(x_), float(y_), float(z_)) for x_, y_, z_ in zip(x.flatten(), y.flatten(), z.flatten())]
-        
-        #pc2 = PointCloud2()
-        #pc2 = PointCloud2.create_cloud_xyz32(header, [x], [y], [z])
-        #pc2 = PointCloud2.create_cloud_xyz32(header, points)
-        
-        #pc2 = sensor_msgs.point_cloud2.create_cloud_xyz32(header,fields, points)
-        #pc2.header = header
-        #pc2.height = 1
-        #pc2.width = len(points)
-        #pc2.is_bigendian = False
-        #pc2.is_dense = False
-        #pc2.fields = fields
-        #pc2.point_step = len(points[0]) * 3 #Correction de 4 a 3
-        #pc2.row_step = pc2.point_step * pc2.width
-        #pc2.data = points.tobytes()
 
         pcl_msg.fields = fields
 
