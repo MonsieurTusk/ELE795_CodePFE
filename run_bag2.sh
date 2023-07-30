9e9e7030ffb2ceaa0219290852ad58633487d1f1
yrodgers <77499322+yrodgers@users.noreply.github.com>
2023-07-30 15:01:16 -0400

    Update run_bag2.sh

--- run_bag2.sh
+++ run_bag2.sh
@@ -2,7 +2,7 @@
 xterm -e "source /opt/ros/humble/setup.bash; ros2 bag play bag_files/bag_lidar_fixe/ --loop" &
 sleep 1
 xterm -e "source /opt/ros/humble/setup.bash; colcon build; source install/setup.bash; ros2 run talker_listener talkerNode_m1m2" &
-sleep 10
+sleep 15
 xterm -e "source /opt/ros/humble/setup.bash; source install/setup.bash; ros2 run talker_listener listenerNode" & 
 sleep 1 
 xterm -e "source /opt/ros/humble/setup.bash; source install/setup.bash; ros2 run talker_listener rapportNode" & 
