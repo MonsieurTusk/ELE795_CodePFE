64aef8dc0f2dd16db0a2b08ee2a8ad45ebcbec9d
yrodgers <77499322+yrodgers@users.noreply.github.com>
2023-07-30 14:52:57 -0400

    Update run_bag.sh
    Ajout de la ligne 8 pour lancer la node rapport

--- run_bag.sh
+++ run_bag.sh
@@ -4,6 +4,8 @@ sleep 1
 xterm -e "source /opt/ros/humble/setup.bash; colcon build; source install/setup.bash; ros2 run talker_listener talkerNode_m1m2" &
 sleep 15
 xterm -e "source /opt/ros/humble/setup.bash; source install/setup.bash; ros2 run talker_listener listenerNode" & 
-sleep 1 
+sleep 1
+xterm -e "source /opt/ros/humble/setup.bash; source install/setup.bash; ros2 run talker_listener rapportNode" & 
+sleep 1
 xterm -e "rviz2 -d talker_listener/PFE_PointCloud2.rviz"
 sleep 1
